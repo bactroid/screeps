96436014764db3ab7b9119e6982ea395a3500804
Kate Haskell <fuzzcat@bactroid.net>
2016-11-27 09:41:39 -0500

    Towers now only heal when more than 50% full of energy

--- towers.js
+++ towers.js
@@ -1,18 +1,5 @@
 module.exports = {
   defendRoom: function (roomName) {
-    function compareByHealth(a,b) {
-      if (a.hits < b.hits) {
-        return -1;
-      }
-
-      if (a.hits > b.hits) {
-        return 1;
-      }
-
-      //otherwise they're equal, so return 0
-      return 0;
-    }
-
     var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
       
     if(hostiles.length > 0) {
@@ -24,19 +11,43 @@ module.exports = {
     }
 
     else {
-      // Get damaged structure
-      var sites = Game.rooms[roomName].find(FIND_STRUCTURES, {
-        filter: obj => obj.hits < (obj.hitsMax / 4) && obj.structureType === STRUCTURE_WALL
-      });
+      // Repair the walls in order of lowest health
+      function repairStructures(tower) {
+        // Used by array.sort to compare damaged structures
+        function compareByHealth(a,b) {
+          if (a.hits < b.hits) {
+            return -1;
+          }
 
-      sites.sort(compareByHealth);
+          if (a.hits > b.hits) {
+            return 1;
+          }
+
+          //otherwise they're equal, so return 0
+          return 0;
+        }
+
+        // Get damaged structure
+        var sites = Game.rooms[roomName].find(FIND_STRUCTURES, {
+          filter: obj => obj.hits < (obj.hitsMax / 4) && obj.structureType === STRUCTURE_WALL
+        });
+
+        sites.sort(compareByHealth);
+
+        percentFull = tower.energy / tower.energyCapacity;
+
+        if (percentFull > 0.5) {
+          tower.repair(sites[0]);
+        }
+      }
 
       //Get list of towers
       var towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {
         filter: {structureType: STRUCTURE_TOWER}
       });
 
-      towers.forEach(tower => tower.repair(sites[0]));
+      //towers.forEach(tower => tower.repair(sites[0]));
+      towers.forEach(repairStructures);
     }
   }
 }
