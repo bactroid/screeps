8a8425203365900e0daedfaf57ee346cbed4f9a9
Kate Haskell <fuzzcat@bactroid.net>
2016-11-27 04:59:25 -0500

    Towers now heal walls with lowest health first

--- towers.js
+++ towers.js
@@ -1,5 +1,18 @@
 module.exports = {
   defendRoom: function (roomName) {
+    function compareByHealth(a,b) {
+      if (a.hits < b.hits) {
+        return -1;
+      }
+
+      if (a.hits > b.hits) {
+        return 1;
+      }
+
+      //otherwise they're equal, so return 0
+      return 0;
+    }
+
     var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
       
     if(hostiles.length > 0) {
@@ -12,16 +25,18 @@ module.exports = {
 
     else {
       // Get damaged structure
-      var site = Game.rooms[roomName].find(FIND_STRUCTURES, {
+      var sites = Game.rooms[roomName].find(FIND_STRUCTURES, {
         filter: obj => obj.hits < (obj.hitsMax / 4) && obj.structureType === STRUCTURE_WALL
       });
+
+      sites.sort(compareByHealth);
 
       //Get list of towers
       var towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, {
         filter: {structureType: STRUCTURE_TOWER}
       });
 
-      towers.forEach(tower => tower.repair(site[0]));
+      towers.forEach(tower => tower.repair(sites[0]));
     }
   }
 }
